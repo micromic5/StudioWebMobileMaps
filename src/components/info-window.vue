4c37b2f8ceced21d873cd19437b39c8a2c4e4bee
micromic5 <mike.amacker@gmx.ch>
2017-10-27 11:01:33 +0200

    Added image comparison slider(buggy inside info window)

--- src/components/info-window.vue
+++ src/components/info-window.vue
@@ -1,21 +1,31 @@
 <template>
-     <div class="iw-container">
+     <div class="iw-container" style="width:500px" >
         <div class="iw-title">{{ title }}</div>
         <div class="iw-description">
             {{ desc }}
         </div>
-        <div class="iw-oldImage"><img :src="oldImage"></div>
-        <div class="iw-newImage"><img :src="newImage"></div>
-        <div class="infoWindowYear">{{year}}</div>
+        <div :id="'id'+id" class="iw-image-slider-container">
+            <img :src="oldImage">
+            <img :src="newImage">
+        </div>
+        <div class="iw-year">{{year}}</div>
     </div>
 </template>
 <script>
+import Vue from "vue"; 
+
 export default{
     name: 'info-window',
     props: ['title', 'desc', 'old-image', 'new-image', 'year'],
     data: function(){
         return {
+            id: null
         }
     },
+    mounted:function(){
+        this.id = this._uid;
+        console.log(this.id);
+        Vue.nextTick(function(){$("#id"+this.id).twentytwenty()}.bind(this));
+    }
 }
 </script>
