8fee3d236c329853ade963cdc35e182ec48f39d6
micromic5 <mike.amacker@gmx.ch>
2017-11-02 14:25:28 +0100

    Revert the Revert madness

--- src/components/contentDiv.vue
+++ src/components/contentDiv.vue
@@ -5,8 +5,8 @@
             {{ desc }}
         </div>
         <div v-if="showSlider!==false" :id="'id'+id" class="image-slider-container">
-            <img :src="oldImage">
-            <img :src="newImage">
+            <img :src="oldImage" @load="imageLoaded">
+            <img :src="newImage" @load="imageLoaded">
         </div>
         <div v-else  class="image-container">
             <img :src="oldImage">
@@ -17,22 +17,30 @@
 </template>
 <script>
 import Vue from "vue";
-
 export default{
     name: 'content-replace-div',
     props: ['title', 'desc', 'old-image', 'new-image', 'year', 'slider'],
     data: function(){
         return {
             id: null,
-            showSlider: false
+            showSlider: false,
+            isImageLoaded: false
+        }
+    },
+    methods:{
+        imageLoaded:function(){
+            if(this.isImageLoaded){
+                if(this.showSlider == true){
+                    this.id = this._uid;
+                 Vue.nextTick(function(){$("#id"+this.id).twentytwenty();}.bind(this));
+                }
+            }else{
+                this.isImageLoaded = true;
+            }
         }
     },
     mounted:function(){
         this.showSlider = (this.slider==="true");
-        if(this.showSlider == true){
-            this.id = this._uid;
-            Vue.nextTick(function(){$("#id"+this.id).twentytwenty()}.bind(this));
-        }
     }
 }
 </script>
--- src/components/map.vue
+++ src/components/map.vue
@@ -4,12 +4,15 @@
 <script>
 import Vue from 'vue'
 import mapStyleModule from '../modules/mapStyle.js'
-
 export default {
   name: "map-comp",
   data: function() {
     return {
-      markersArray:[]
+      markersArray:[],      
+      activIcon: contentfulClient.getAsset("QayoeFwEQSYOMaUigSW46")
+        .then(entry => {return entry.fields.file}),
+      passivIcon:contentfulClient.getAsset("4Q18909usM8WkQyy4GI2MI")
+        .then(entry => {return entry.fields.file})
     }
   },
   mounted: function() {
@@ -42,42 +45,44 @@ export default {
     
     getMapContetn: function(){
       const cms_markers = [];
-      contentfulClient
-        .getEntries({
-          content_type: "timeSpots"
-        })
-        .then(entries => {
-          // fuer jeden Eintrag einen Marker und ein Infowindow erstellen
-          entries.items.forEach((item, index) => {
-            // wenn kein Icon definiert ist, den Default verwenden
-            let icon = item.fields.icon ? item.fields.icon.fields.file : "";
-            //create marker
-            let marker = new google.maps.Marker({
-              position: { lat: item.fields.location.lat, lng: item.fields.location.lon },
-              icon: icon,
-              map: this.map
+      this.passivIcon.then(iconPassiv => {
+        contentfulClient
+          .getEntries({
+            content_type: "timeSpots"
+          })
+          .then(entries => {          
+            // fuer jeden Eintrag einen Marker und ein Infowindow erstellen
+            entries.items.forEach((item, index) => {
+              // wenn kein Icon definiert ist, den Default verwenden
+              let icon = item.fields.icon ? item.fields.icon.fields.file : "";
+              //create marker
+              let marker = new google.maps.Marker({
+                position: { lat: item.fields.location.lat, lng: item.fields.location.lon },
+                icon: iconPassiv,
+                map: this.map
+              });
+              this.markersArray.push(marker);
+              // Beim Klick auf den Marker wird der Inhalt des content-div mit dem Content ersetzt
+              marker.addListener('click', event => {
+                this.activIcon.then(newIcon =>{marker.icon = icon});
+                this.map.panTo({lat:marker.position.lat(),lng:marker.position.lng()-.014});
+                $("#content-div").html(`<content-replace-div id="content-replace" title="${item.fields.title}" desc="${item.fields.description}"
+                  old-image="${item.fields.oldPicture.fields.file.url}" new-image="${item.fields.newPicture.fields.file.url}"
+                  year="${item.fields.year}" slider="${item.fields.slider}"></content-replace-div>`);
+                  if($("#content-div").attr('class')!="open"){
+                    setTimeout(function(){new Vue().$mount(`#content-replace`)},3001);
+                    $("#content-div").css({"overflow": "visible"});
+                    $("#content-div").attr('class', 'open');
+                    $("#content-div").css({"width": "83vw"});
+                  }else{
+                    new Vue().$mount(`#content-replace`);
+                  }
+              });
+              cms_markers.push(marker);
             });
-            this.markersArray.push(marker);
-            // Beim Klick auf den Marker wird der Inhalt des content-div mit dem Content ersetzt
-            marker.addListener('click', event => {
-              this.map.panTo({lat:marker.position.lat(),lng:marker.position.lng()-.014});
-              $("#content-div").html(`<content-replace-div id="content-replace" title="${item.fields.title}" desc="${item.fields.description}"
-                old-image="${item.fields.oldPicture.fields.file.url}" new-image="${item.fields.newPicture.fields.file.url}"
-                year="${item.fields.year}" slider="${item.fields.slider}"></content-replace-div>`);
-                if($("#content-div").attr('class')!="open"){
-                  setTimeout(function(){new Vue().$mount(`#content-replace`)},3001);
-                  $("#content-div").css({"overflow": "visible"});
-                  $("#content-div").attr('class', 'open');
-                  $("#content-div").css({"width": "83vw"});
-                }else{
-                  new Vue().$mount(`#content-replace`);
-                }
-            });
-            cms_markers.push(marker);
           });
         });
     },
-
     insertFirstContent: function(){
        contentfulClient.getAsset("5wutUrRRPUC8wg6CiMOW0W")
       .then(entry => {
