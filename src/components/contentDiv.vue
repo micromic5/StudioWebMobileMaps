7390d3d5f56a30aa059270406c2dd22f528023f5
Daniel <hello@scotchline.ch>
2017-11-03 10:26:02 +0100

    css änderungen

--- src/components/contentDiv.vue
+++ src/components/contentDiv.vue
@@ -1,35 +1,38 @@
 <template>
    
    <div class="grid-container">
+   
 
       <div class="grid-x grid-padding-x">
         <div class="large11 cell">
           <h1>{{ title }}</h1>
-          <!-- Grid Example -->
         </div>
       </div>
 
        <div class="grid-x grid-padding-x text">
-            <div class="large-10 large-offset-2 cell">
+            <div class="large-4  cell"></div>
+            <div class="large-8  cell">
                 <p> {{ desc }}</p>
             </div>
        </div>
 
-         <div class="grid-x grid-padding-x">
-            <div class="large-offset-2 large-10 cell">
-              <div class="border-image">
-                
-
-                  
-
-
-              </div>
+    <div class="grid-x grid-padding-x">
+        <div class="large-offset-2 large-10 cell">
+            <div class="border-image">
+            <div v-if="showSlider!==false" :id="'id'+id" class="image-slider-container">
+                <img :src="oldImage" @load="imageLoaded" width="80%">
+                <img :src="newImage" @load="imageLoaded">
             </div>
-                       
+            <div v-else  class="image-container">
+                <img :src="oldImage">
+                <img :src="newImage">
+            </div>
+            </div>
+        </div>
+    </div>
 
-</div>
-       
-       
+
+   
         <div v-if="yearDisabled" class="year">{{year}}</div>
     </div>
 </template>
