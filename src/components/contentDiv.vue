c1feea53654b6a3f4757d87c0ac6131320b9e927
Daniel <hello@scotchline.ch>
2017-11-03 13:04:55 +0100

    Style Web

--- src/components/contentDiv.vue
+++ src/components/contentDiv.vue
@@ -29,38 +29,46 @@
         </div>
     </div>
 
-    <div class="grid-x grid-padding-x kommentar">
-        <div class="large-offset-3 large-9 medium-12 cell">
+    <div class="grid-x comments">
+         <div class="large-4 cell"></div>
+            <div class="large-9 cell">
           <h3>Persönliche Geschichten</h3>
         </div>
-         <div class="large-9 large-offset-3 cell">
-	         <h5>Ramon Rieser</h5>
-			    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lacinia bibendum nulla sed consectetur. Nulla vitae elit libero, a pharetra augue. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum facilis tenetur a voluptatibus quia
-			      deserunt.
-			    </p>
+          <div class="large-2 cell"></div>
+            <div class="large-8 cell">
+	         <h4>Ramon Rieser</h4>
+			   <p class="kommentar">Eine wunderbare Heiterkeit hat meine ganze Seele eingenommen, gleich den süßen Frühlingsmorgen, die ich mit ganzem Herzen genieße. Ich bin allein und freue mich meines Lebens in dieser Gegend, die für solche Seelen geschaffen ist wie die meine. Ich bin so glücklich, mein Bester, so ganz in dem Gefühle von ruhigem Dasein versunken, daß meine Kunst darunter leidet.</p>  
 
-                
+                  <hr>
+                  <h4>Max Muster</h4>
+			   <p class="kommentar">Ich könnte jetzt nicht zeichnen, nicht einen Strich, und bin nie ein größerer Maler gewesen als in diesen Augenblicken. Wenn das liebe Tal um mich dampft, und die hohe Sonne an der Oberfläche der undurchdringlichen Finsternis meines Waldes ruht, und nur einzelne Strahlen sich in das innere Heiligtum stehlen, ich dann im hohen Grase am fallenden Bache liege, und näher an der Erde tausend mannigfaltige Gräschen mir merkwürdig. <br> Wenn ich das Wimmeln der kleinen Welt zwischen Halmen, die unzähligen, unergründlichen Gestalten der Würmchen, der Mückchen näher an meinem Herzen fühle, und fühle die Gegenwart des Allmächtigen, der uns nach seinem Bilde schuf, das Wehen des Alliebenden, der uns in ewiger Wonne schwebend trägt und erhält; mein Freund! </p> 
         </div>
   </div> 
 
-<div class="grid-x grid-padding-x">
-	<div class="large-4 large-offset-3 cell">
-	</div>
-      <div class="large-6 large-offset-3 cell">
+<div class="grid-x formular">
+         <div class="large-4 cell"></div>
+            <div class="large-9 cell">
 	      <h3>Haben auch Sie eine Geschichte auf Lager?</h3>
-        <label>Name
-          <input type="text">
-        </label>
-        <label>E-Mail
-          <input type="text">
-        </label>
-        <label>Geschichte
-          <textarea rows="10" type="text"></textarea>
-        </label>
-        <button class="button expanded">Versenden</button>
+         </div>
 </div>
 
+
+
+<div class="grid-x formular">
+        <div class="large-4 cell">Name</div>
+        <div class="large-8 cell"><input type="text"></div>
 </div>
+
+<div class="grid-x formular">
+        <div class="large-4 cell">E-Mail</div>
+        <div class="large-8 cell"><input type="text"></div>
+</div>
+
+<div class="grid-x formular">
+        <div class="large-4 cell">Geschichte</div>
+        <div class="large-8 cell"><textarea rows="10" type="text"></textarea></div>
+</div>
+        <button class="button expanded">Versenden</button>
 
    
         <div v-if="yearDisabled" class="year">{{year}}</div>
